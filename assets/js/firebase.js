d2d3427680ed4a7e11d21cd57a0aa235c6db2053
BabyJace2014 <35673340+BabyJace2014@users.noreply.github.com>
2018-07-07 21:19:14 -0400

    Started

---
+++ assets/js/firebase.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCGuFNJf5nNGu6aOePMwJDVWC27Tys0QC0",
+    authDomain: "rps-multiplayer-82632.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-82632.firebaseio.com",
+    projectId: "rps-multiplayer-82632",
+    storageBucket: "rps-multiplayer-82632.appspot.com",
+    messagingSenderId: "348964508384"
+  };
+  firebase.initializeApp(config);
